2fcaca81dcd1aca5f5e2892c4e6c36ed01abf65a
Mathew Topper <damm_horse@yahoo.co.uk>
2017-03-27 17:43:40 +0100

    Implement ToolManager as part of the extensions module and create a Tool plugin to plot the constraints plot from the electrical module.

--- dtocean_core/strategies/__init__.py
+++ dtocean_core/strategies/__init__.py
@@ -15,13 +15,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import abc
-import json
-import pickle
 from copy import deepcopy
 
-from aneris.boundary.data import SerialBox
 from polite.abc import abstractclassmethod
 
 from ..menu import ModuleMenu, ThemeMenu
