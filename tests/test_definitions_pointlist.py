615c91a39a11baf3741ab0f221806b9b291e9dcb
Mathew Topper <damm_horse@yahoo.co.uk>
2017-06-22 18:33:19 +0100

    Fix pointlist input and output definitions and test.

--- tests/test_definitions_pointlist.py
+++ tests/test_definitions_pointlist.py
@@ -3,7 +3,9 @@ import pytest
 import matplotlib.pyplot as plt
 
 from aneris.control.factory import InterfaceFactory
-from dtocean_core.core import (AutoPlot,
+from dtocean_core.core import (AutoFileInput,
+                               AutoFileOutput,
+                               AutoPlot,
                                Core)
 from dtocean_core.data import CoreMetaData
 from dtocean_core.data.definitions import PointList
@@ -55,6 +57,54 @@ def test_PointList():
     
     with pytest.raises(ValueError):
         test.get_data(raw, meta)
+        
+        
+@pytest.mark.parametrize("fext", [".csv", ".xls", ".xlsx"])
+def test_PointList_auto_file(tmpdir, fext):
+
+    test_path = tmpdir.mkdir("sub").join("test{}".format(fext))
+    test_path_str = str(test_path)
+           
+    raws = [[(0., 0.),
+             (1., 1.),
+             (2., 2.)], 
+            [(0., 0., 0.),
+             (1., 1., 1.),
+             (2., 2., 2.)]]
+    
+    ztests = [False, True]
+    
+    for raw, ztest in zip(raws, ztests):
+    
+        meta = CoreMetaData({"identifier": "test",
+                             "structure": "test",
+                             "title": "test"})
+    
+        test = PointList()
+        
+        fout_factory = InterfaceFactory(AutoFileOutput)
+        FOutCls = fout_factory(meta, test)
+        
+        fout = FOutCls()
+        fout._path = test_path_str
+        fout.data.result = test.get_data(raw, meta)
+    
+        fout.connect()
+        
+        assert len(tmpdir.listdir()) == 1
+                  
+        fin_factory = InterfaceFactory(AutoFileInput)
+        FInCls = fin_factory(meta, test)
+                  
+        fin = FInCls()
+        fin._path = test_path_str
+        
+        fin.connect()
+        result = test.get_data(fin.data.result, meta)
+        
+        assert result[0].x == 0
+        assert result[1].y == 1
+        assert result[0].has_z == ztest
         
 
 def test_PointList_auto_plot(tmpdir):
