731f24d76ab4e0ff5f1b56fc9d4288fe27a4f311
Mathew Topper <damm_horse@yahoo.co.uk>
2018-01-05 17:27:46 +0000

    Add plot for single device power matrix

--- dtocean_core/interfaces/plots_wave.py
+++ dtocean_core/interfaces/plots_wave.py
@@ -135,6 +135,118 @@ class WaveOccurrencePlot(PlotInterface):
         self.fig_handle = plt.gcf()
 
         return
+    
+    
+class PowerMatrixPlot(PlotInterface):
+
+    @classmethod
+    def get_name(cls):
+
+        '''A class method for the common name of the interface.
+
+        Returns:
+          str: A unique string
+        '''
+
+        return "Single Wave Device Power Matrix"
+
+    @classmethod
+    def declare_inputs(cls):
+
+        '''A class method to declare all the variables required as inputs by
+        this interface.
+
+        Returns:
+          list: List of inputs identifiers
+
+        Example:
+          The returned value can be None or a list of identifier strings which
+          appear in the data descriptions. For example::
+
+              inputs = ["My:first:variable",
+                        "My:second:variable",
+                       ]
+        '''
+
+        input_list = ['device.wave_power_matrix']
+
+        return input_list
+
+    @classmethod
+    def declare_id_map(self):
+
+        '''Declare the mapping for variable identifiers in the data description
+        to local names for use in the interface. This helps isolate changes in
+        the data description or interface from effecting the other.
+
+        Returns:
+          dict: Mapping of local to data description variable identifiers
+
+        Example:
+          The returned value must be a dictionary containing all the inputs and
+          outputs from the data description and a local alias string. For
+          example::
+
+              id_map = {"var1": "My:first:variable",
+                        "var2": "My:second:variable",
+                        "var3": "My:third:variable"
+                       }
+
+        '''
+
+        id_map = {"power_matrix": 'device.wave_power_matrix'
+                  }
+
+        return id_map
+
+    def connect(self):
+
+        occurrence_grid = self.data.occurrence_matrix
+        occurrence_flat = occurrence_grid.sum("Dir")
+        vals = occurrence_flat.values.T
+    
+        col_normals = Normalize(vals.min(), vals.max())
+    
+        cellStrs = []
+    
+        for row in vals:
+            rowStrs = ["{0:d}".format(int(x)) for x in row]
+            cellStrs.append(rowStrs)
+    
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111,
+                              frameon=True,
+                              xticks=[],
+                              yticks=[])
+    
+        # Add headers and a table at the bottom of the axes
+        header_0 = plt.table(cellText=[['']],
+                             colLabels=['Te [$s$]'],
+                             loc='center',
+                             bbox=[0, 0.85, 1.0, 0.1]
+                             )
+    
+        header_1 = plt.table(cellText=[['Bob']],
+                             rowLabels=['Hm0 [$m$]          '],
+                             loc='center',
+                             bbox=[0, 0, 1, 0.8575]
+                             )
+        
+        short_hm0 = ["{:.2f}".format(x) for x in occurrence_flat.Hm0.values]
+        short_te = ["{:.2f}".format(x) for x in occurrence_flat.Te.values]
+    
+        the_table = plt.table(cellText=cellStrs,
+                              rowLabels=short_hm0,
+                              colLabels=short_te,
+                              cellColours=plt.cm.RdYlGn_r(col_normals(vals)),
+                              loc='center',
+                              bbox=[0, 0, 1, 0.9])
+    
+        plt.title("Single Device Power Matrix (Over All Directions) [kW]")
+
+        self.fig_handle = plt.gcf()
+
+        return
 
 
 class TeHm0Plot(PlotInterface):
