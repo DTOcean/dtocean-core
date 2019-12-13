a710ef5e12678f26ae28a78217b5a15f2c862ad4
Mathew Topper <damm_horse@yahoo.co.uk>
2019-12-13 12:31:03 +0000

    PEP8

--- dtocean_core/utils/execute.py
+++ dtocean_core/utils/execute.py
@@ -37,7 +37,13 @@ def warn_with_traceback(message,
 
     log = file if hasattr(file,'write') else sys.stderr
     traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
+    log.write(warnings.formatwarning(message,
+                                     category,
+                                     filename,
+                                     lineno,
+                                     line))
+    
+    return
 
 
 def main(fpath,
